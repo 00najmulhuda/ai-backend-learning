a1caed8c9c4967baf26dd452edae024227c0b40a
Najmul Huda <nh5037480@gmail.com>
2026-07-18 00:54:58 +0530

    Integrate Ollama and implement LLM parameters

--- app/main.py
+++ app/main.py
@@ -4,10 +4,26 @@ response = ollama.chat(
     model = "qwen2.5:3b",
     messages =  [
         {
+            "role":"system",
+            "content":"you are beginer freindly python teacher"
+        },
+        {
             "role":"user",
-            "content":"explain FastAPI is simplete words and in short"
+            "content":"whatis fastapi"
+        },
+        {
+            "role":"assistant",
+            "content":"FastAPI is modern python framework used to build API"
+        },
+        {
+            "role":"user",
+            "content":"who created it"
         }
-    ]
+    ],
+    options = {
+        "temperature":1.0,
+        "num_predict":300
+    }
 )
 
 print(response["message"]["content"])
